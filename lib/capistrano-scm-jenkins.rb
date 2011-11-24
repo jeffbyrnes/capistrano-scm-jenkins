acd166b7cc920dd3e2caccac1c8e34e3ab29d128
LI Daobing <lidaobing@gmail.com>
2011-11-24 15:43:15 +0800

    init capistrano-scm-jenkins

---
+++ lib/capistrano-scm-jenkins.rb
@@ -0,0 +1,9 @@
+require "capistrano-scm-jenkins/version"
+
+module Capistrano
+  module Scm
+    module Jenkins
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/capistrano-scm-jenkins/version.rb
@@ -0,0 +1,7 @@
+module Capistrano
+  module Scm
+    module Jenkins
+      VERSION = "0.0.1"
+    end
+  end
+end
