8ab55ed27e34538281e83b5130d7909f4b743ddd
LI Daobing <lidaobing@gmail.com>
2012-06-21 22:50:57 +0800

    ready to release 0.0.4

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+## 0.0.4 (2012-06-21)
+
+* working with non-English server (thanks cynipe).
+
 ## 0.0.3 (2011-12-13)
 
 * support netrc
--- lib/capistrano-scm-jenkins/version.rb
+++ lib/capistrano-scm-jenkins/version.rb
@@ -1,7 +1,7 @@
 module Capistrano
   module Scm
     module Jenkins
-      VERSION = "0.0.3"
+      VERSION = "0.0.4"
     end
   end
 end
