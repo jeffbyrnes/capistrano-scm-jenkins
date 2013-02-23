9a56987e3104b80f140c97c797ef77520e9ec2d5
Vadim Vasciuc <vashciuc@gmail.com>
2013-02-23 11:16:31 +0200

    out directory creation fix

--- lib/capistrano/recipes/deploy/scm/jenkins.rb
+++ lib/capistrano/recipes/deploy/scm/jenkins.rb
@@ -33,7 +33,7 @@ module Capistrano
           execute << 'cd $TMPDIR'
           execute << "curl #{curl_interface} #{insecure} #{authentication} -sO '#{artifact_zip_url(revision)}'"
           if variable(:jenkins_artifact_path)
-            execute << "unzip archive.zip -d \"./out/\""
+            execute << 'unzip archive.zip -d \'out/\''
             execute << "mv out/#{variable(:jenkins_artifact_path)} #{destination}"
           else
             execute << "unzip archive.zip -d \"#{destination}\""
