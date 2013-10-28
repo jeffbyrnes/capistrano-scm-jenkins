e1e59ef928fba6d8fdf6075a27630b2d586e4cb0
LI Daobing <lidaobing@gmail.com>
2013-10-28 17:50:12 +0800

    fix #9

--- spec/capistrano/recipes/deploy/scm/jenkins_spec.rb
+++ spec/capistrano/recipes/deploy/scm/jenkins_spec.rb
@@ -34,8 +34,19 @@ module Capistrano::Deploy::SCM
         "account #22 (1 more test is failing (total 19))" => 22,
         "account #37 (stable)" => 37
       }.each do |text, build_number|
-        it "should convert `#{text}` to `#{build_number}" do
+        it "should convert `#{text}` to `#{build_number}`" do
           @jenkins.send(:get_build_number_from_rss_all_title, text).should == "#{build_number}"
+        end
+      end
+    end
+
+    context 'strip_right_parentheses' do
+      {
+        "account #22 (1 more test is failing (total 19))" => "account #22 ",
+        "account #37 (stable)" => "account #37 "
+      }.each do |from, to|
+        it "should convert `#{from}` to `#{to}" do
+          @jenkins.send(:strip_right_parentheses, from).should == to
         end
       end
     end
